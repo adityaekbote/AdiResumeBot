0472890a4c6a20ade107cdc05a87098baad9a2b6
Aditya Ekbote <ekbote.aditya@gmail.com>
2016-07-09 11:13:41 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('So you want to learn about Esther? Just say HELLO to get started.')
+            return bot.say('Hi, I am the personal chatbot for Aditya Ekbote.Just say HI to get started.')
                 .then(() => 'speak');
         }
     },
