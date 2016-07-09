d7cf9733109c4f60c6687d9f8914df081c509b7d
Aditya Ekbote <ekbote.aditya@gmail.com>
2016-07-09 11:19:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,17 @@ module.exports = new Script({
 
     start: {
         receive: (bot) => {
-            return bot.say('Hi, I am the personal chatbot for Aditya Ekbote.Just say HI to get started.')
+            return bot.say('Hi, I am the personal chatbot for Aditya Ekbote.Lets get started!')
+                .then(() => 'askName');
+        }
+    },
+    
+    askName: {
+        prompt: (bot) => bot.say('What\'s your name?'),
+        receive: (bot, message) => {
+            const name = message.text;
+            return bot.setProp('name', name)
+                .then(() => bot.say(`Great! I'll call you ${name}`))
                 .then(() => 'speak');
         }
     },
